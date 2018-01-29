bd6090d1773f1f8ed9815fc4b9c0fd73d94a61a2
aurelius0523 <aurelius0523@gmail.com>
2018-01-29 21:06:14 +0800

    moved settings to base file

--- settings/base.py
+++ settings/base.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*v(7s64%76he74bgvgoj2_q(mh#7km^tco0@500gqo@ap#)(2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysterious-thicket-84893.herokuapp.com']
 
 # Application definition
 
@@ -114,6 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # graphene schema
 GRAPHENE = {
